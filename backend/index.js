3023ef9f1f3c21f8460194a1a6c02f42a33c87c4
ROOPEESH_KUMAR <128828453+roopeshkumar11@users.noreply.github.com>
2024-06-12 01:42:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app=express()
 app.use(express.json())
 app.use(cors(
     {
-        origin:["https://frontend-web-lime.vercel.app/"],
+        origin:["https://frontend-web-lime.vercel.app"],
          method:["POST","GET"],
         credentials:true
 )); 
